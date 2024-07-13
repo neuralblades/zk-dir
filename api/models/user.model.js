460f51724768a0b88e83a9f3dffa26ccc7b6dbdb
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-07-13 23:24:51 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,6 +11,10 @@ const  userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    password: {
+        type: String,
+        required: true,
+    },
 }, {timestamps: true}
 );
 
