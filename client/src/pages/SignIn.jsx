074a83edb6732c52489e6bc66b329817d18ab7b0
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-08-13 15:20:17 +0400

    logo hidden

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -41,7 +41,7 @@ export default function SignIn() {
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
-        <div className='flex-1 self-center'>
+        <div className='flex-1 self-center hidden sm:block'>
           <Link to='/'>
             <img src="img/logoz.png" alt="#" />
           </Link>
