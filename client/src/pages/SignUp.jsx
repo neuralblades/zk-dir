e84b07651aa64ea234834278441a4ff475df12be
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-08-13 14:23:09 +0400

    updated sign in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -42,20 +42,10 @@ export default function SignUp() {
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
-        <div className='flex-1'>
-          <Link
-            to='/'
-            className='font-bold dark:text-white text-4xl'
-          >
-            <span className='px-2 py-1 bg-gradient-to-r from-green-500 via-green-400 to-green-500 rounded-lg text-white'>
-              ZK-Bug
-            </span>
-            Directory
+        <div className='flex-1 self-center'>
+          <Link to='/'>
+            <img src="img/logoz.png" alt="#" />
           </Link>
-          <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-            or with Google.
-          </p>
         </div>
         {/* right */}
 
