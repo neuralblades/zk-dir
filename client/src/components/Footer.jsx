633f3455183b853418e6fde51c1fa9b4aa8f8349
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-08-28 18:56:44 +0400

    footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,75 +1,28 @@
-import { Footer } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function FooterCom() {
   return (
-    <Footer container className='border border-t-8 border-gray-300'>
-      <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
-          <div className='mt-5 w-[70px]'>
-            <Link to='/'>
-              <img src="img/logoz.png" alt="#" />
-            </Link>
-          </div>
-          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
-            <div>
-              <Footer.Title title='About' />
-              <Footer.LinkGroup col>
-                <Footer.Link
-                  href=''
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Learn about ZK
-                </Footer.Link>
-                <Footer.Link
-                  href='/about'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  ZK Bug Directory
-                </Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
-                <Footer.Link
-                  href=''
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Github
-                </Footer.Link>
-                <Footer.Link href='#'>Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Legal' />
-              <Footer.LinkGroup col>
-                <Footer.Link href='#'>Privacy Policy</Footer.Link>
-                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-          </div>
-        </div>
-        <Footer.Divider />
-        <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <Footer.Copyright
-            href='#'
-            by="ZK Bug Directory"
-            year={new Date().getFullYear()}
-          />
-          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href='#' icon={BsFacebook}/>
-            <Footer.Icon href='#' icon={BsInstagram}/>
-            <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='#' icon={BsGithub}/>
-            <Footer.Icon href='#' icon={BsDribbble}/>
-
-          </div>
-        </div>
+    <footer className="bg-gray-950 text-gray-400 text-xs py-2 px-4 flex justify-between items-center border border-gray-700">
+      <div>
+        {new Date().getFullYear()} © ZKBD
       </div>
-    </Footer>
+      <div className="flex space-x-4 items-center">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+          <FaTwitter size={18} />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+          <FaGithub size={18} />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+          <FaLinkedin size={18} />
+        </a>
+        <button className="ml-4 bg-gray-700 hover:bg-gray-600 text-white px-2 py-1 rounded">
+          Feedback
+        </button>
+      </div>
+      <div className="absolute left-1/2 transform -translate-x-1/2 text-sm">
+        Created by Nullity
+      </div>
+    </footer>
   );
 }
