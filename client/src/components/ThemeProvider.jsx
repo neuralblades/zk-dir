8e498fd2fdc59eab49202515d8a7cb09f7c81ee9
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-08-28 18:57:27 +0400

    space remove

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,9 +1,6 @@
 import { useSelector } from "react-redux"
 import PropTypes from 'prop-types';
 
-
-
-
 export default function ThemeProvider({ children }) {
     const {theme} = useSelector(state => state.theme)
   return (
