3e616ba7b2040b3c54d12f0b9e5c4bf186ce169c
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-08-30 05:11:48 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function FooterCom() {
   return (
-    <footer className="bg-gray-950 text-gray-400 text-xs py-2 px-4 flex justify-between items-center border border-gray-700">
+    <footer className="bg-black text-gray-400 text-xs py-2 px-4 flex justify-between items-center border-t border-gray-900">
       <div>
         {new Date().getFullYear()} © ZKBD
       </div>
@@ -16,9 +16,6 @@ export default function FooterCom() {
         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
           <FaLinkedin size={18} />
         </a>
-        <button className="ml-4 bg-gray-700 hover:bg-gray-600 text-white px-2 py-1 rounded">
-          Feedback
-        </button>
       </div>
       <div className="absolute left-1/2 transform -translate-x-1/2 text-sm">
         Created by Nullity
