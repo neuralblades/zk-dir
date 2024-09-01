809c1de496090da8871757c7c0592b25f26a68c0
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-09-01 13:14:00 +0530

    theme b/w update

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,7 @@ export default function ThemeProvider({ children }) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(11,14,27)] min-h-screen">
+        <div className="bg-black text-gray-700 dark:text-gray-200 dark:bg-black min-h-screen">
             {children}
         </div>
     </div>
