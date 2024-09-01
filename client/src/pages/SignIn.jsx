7ba811614a18ee7c4436dfb8579c6efdfb51bbb3
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-09-01 17:10:03 +0530

    signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,22 +1,56 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
+const InputField = ({ label, type, placeholder, id, onChange }) => (
+  <div className="mb-4">
+    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
+      {label}
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 bg-zinc-900 text-gray-700 leading-tight focus:outline-none focus:ring-1 focus:ring-zinc-900 focus:border-violet-950"
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      onChange={onChange}
+    />
+  </div>
+);
+
+const Button = ({ type, disabled, children }) => (
+  <button
+    className={` w-full bg-violet-950 hover:bg-zinc-950 border border-violet-950 text-white font-bold py-2 px-4 rounded focus:outline-none ${
+      disabled ? 'opacity-50 cursor-not-allowed' : ''
+    }`}
+    type={type}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+);
+
+const Alert = ({ children }) => (
+  <div className=" text-red-700 px-4 py-3 text-center relative" role="alert">
+    <span className="block sm:inline">{children}</span>
+  </div>
+);
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading, error: errorMessage} = useSelector(state => state.user);
+  const { loading, error: errorMessage } = useSelector(state => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return dispatch(signInFailure('Please fill out all the feilds'));
+      return dispatch(signInFailure('Please fill out all the fields'));
     }
     try {
       dispatch(signInStart());
@@ -29,7 +63,7 @@ export default function SignIn() {
       if (data.success === false) {
         dispatch(signInFailure(data.message));
       }
-      if(res.ok) {
+      if (res.ok) {
         dispatch(signInSuccess(data));
         navigate('/');
       }
@@ -37,65 +71,45 @@ export default function SignIn() {
       dispatch(signInFailure(error.message));
     }
   };
+
   return (
-    <div className='h-[89vh] bg-black'>
-      <div className='flex p-3 pt-20 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-        {/* left */}
-        <div className='flex-1 self-center hidden sm:block'>
-          <Link to='/'>
-            <img src="img/logoz.png" alt="#" />
+    <div className='min-h-[87vh] flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-md w-full space-y-8'>
+        <div>
+          <img className="mx-auto shadow-bg h-12 w-auto" src="/img/logozk3.png" alt="Logo" />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-300">
+            Sign in to your account
+          </h2>
+        </div>
+        <form className='mt-8 space-y-6' onSubmit={handleSubmit}>
+          <InputField
+            label="Your email"
+            type="email"
+            placeholder="name@company.com"
+            id="email"
+            onChange={handleChange}
+          />
+          <InputField
+            label="Your password"
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={handleChange}
+          />
+          <Button type="submit" disabled={loading}>
+            {loading ? 'Loading...' : 'Sign In'}
+          </Button>
+          <OAuth />
+        </form>
+        <div className='flex gap-2 text-sm mt-5 justify-center'>
+          <span>Don't have an account?</span>
+          <Link to='/sign-up' className='text-blue-500'>
+            Sign Up
           </Link>
         </div>
-        {/* right */}
-
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-            <div>
-              <Label value='Your email' />
-              <TextInput
-                type='email'
-                placeholder='name@company.com'
-                id='email'
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <Label value='Your password' />
-              <TextInput
-                type='password'
-                placeholder='Password'
-                id='password'
-                onChange={handleChange}
-              />
-            </div>
-            <Button
-              gradientMonochrome="success"
-              type='submit'
-              disabled={loading}
-            >
-              {loading ? (
-                <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
-                </>
-              ) : (
-                'Sign In'
-              )}
-            </Button>
-            <OAuth />
-          </form>
-          <div className='flex gap-2 text-sm mt-5'>
-            <span>Dont Have an account?</span>
-            <Link to='/sign-up' className='text-blue-500'>
-              Sign Up
-            </Link>
-          </div>
-          {errorMessage && (
-            <Alert className='mt-5' color='failure'>
-              {errorMessage}
-            </Alert>
-          )}
-        </div>
+        {errorMessage && (
+          <Alert>{errorMessage}</Alert>
+        )}
       </div>
     </div>
   );
