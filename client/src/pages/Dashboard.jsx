808551ab1159a37966fb79221fc3931f1086552e
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-09-01 13:14:32 +0530

    dashboard upadate

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className='min-h-[87vh] flex flex-col md:flex-row'>
       <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
