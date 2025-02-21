2946a7625a5f3406f66e7518e3373eeda22be495
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-02-21 16:56:13 +0400

    Sign in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,11 +6,11 @@ import OAuth from '../components/OAuth';
 
 const InputField = ({ label, type, placeholder, id, onChange }) => (
   <div className="mb-4">
-    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
+    <label className="block text-gray-300 text-sm font-bold mb-2" htmlFor={id}>
       {label}
     </label>
     <input
-      className="shadow appearance-none border rounded w-full py-2 px-3 bg-zinc-900 text-gray-700 leading-tight focus:outline-none focus:ring-1 focus:ring-zinc-900 focus:border-violet-950"
+      className="shadow appearance-none border rounded w-full py-2 px-3 bg-zinc-900 text-gray-200 leading-tight focus:outline-none focus:ring-1 focus:ring-zinc-900 focus:border-gray-950"
       type={type}
       placeholder={placeholder}
       id={id}
@@ -21,7 +21,7 @@ const InputField = ({ label, type, placeholder, id, onChange }) => (
 
 const Button = ({ type, disabled, children }) => (
   <button
-    className={` w-full bg-violet-950 hover:bg-zinc-950 border border-violet-950 text-white font-bold py-2 px-4 rounded focus:outline-none ${
+    className={` w-full bg-gray-200 hover:bg-zinc-100 transition delay-100 duration-300 border border-blue-800 text-black font-bold py-2 px-4 rounded focus:outline-none ${
       disabled ? 'opacity-50 cursor-not-allowed' : ''
     }`}
     type={type}
@@ -76,9 +76,9 @@ export default function SignIn() {
     <div className='min-h-[87vh] flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-md w-full space-y-8'>
         <div>
-          <img className="mx-auto shadow-bg h-12 w-auto" src="/img/logozk3.png" alt="Logo" />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-300">
-            Sign in to your account
+          <img className="mx-auto h-12 w-auto" src="/img/logozk1.png" alt="Logo" />
+          <h2 className="mt-6 text-center font-bold text-3xl text-gray-100">
+            LOGIN
           </h2>
         </div>
         <form className='mt-8 space-y-6' onSubmit={handleSubmit}>
@@ -102,7 +102,7 @@ export default function SignIn() {
           <OAuth />
         </form>
         <div className='flex gap-2 text-sm mt-5 justify-center'>
-          <span>Don't have an account?</span>
+          <span>Don&apos;t have an account?</span>
           <Link to='/sign-up' className='text-blue-500'>
             Sign Up
           </Link>
