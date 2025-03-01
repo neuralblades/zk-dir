f3f676d31bbc3e07693b10316b80ddbb1c368818
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-03-01 23:56:50 +0400

    dash post

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -79,7 +79,7 @@ export default function DashPosts() {
               <Table.HeadCell>Date updated</Table.HeadCell>
               <Table.HeadCell>Post image</Table.HeadCell>
               <Table.HeadCell>Post title</Table.HeadCell>
-              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Protocol</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
               <Table.HeadCell>
                 <span>Edit</span>
@@ -108,7 +108,7 @@ export default function DashPosts() {
                       {post.title}
                     </Link>
                   </Table.Cell>
-                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>{post.protocol?.name || 'N/A'}</Table.Cell>
                   <Table.Cell>
                     <span
                       onClick={() => {
