df5b2bcc0b73059bb3c3a1c503322666616ddbf5
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-03-01 23:51:24 +0400

    post route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -6,9 +6,9 @@ import {
   getposts, 
   updatepost,
   getPostsByProtocol,
-  getPostsByCategory,
   getPostStats,
-  getPostBySlug
+  getPostBySlug,
+  getLanguages
 } from '../controllers/post.controller.js';
 
 const router = express.Router();
@@ -20,10 +20,10 @@ router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
 router.put('/updatepost/:postId/:userId', verifyToken, updatepost);
 
 // New routes for ZK bug directory
+router.get('/languages', getLanguages);
 router.get('/by-audit-firm/:firmName', getposts);
 router.get('/by-date-range', getposts);
 router.get('/protocol/:protocolName', getPostsByProtocol);
-router.get('/category/:categoryName', getPostsByCategory);
 router.get('/stats', getPostStats);
 router.get('/post/:slug', getPostBySlug);
 
@@ -31,9 +31,8 @@ router.get('/post/:slug', getPostBySlug);
 router.get('/search-params', (req, res) => {
   res.json({
     protocolTypes: ['ZKEVM', 'ZK-ROLLUP', 'OTHER'],
-    severityLevels: ['low', 'medium', 'high', 'critical'],
-    difficultyLevels: ['low', 'medium', 'high'],
-    categories: ['vulnerabilities', 'bugs', 'optimizations', 'uncategorized'],
+    severityLevels: ['N/A', 'informational', 'low', 'medium', 'high', 'critical'],
+    difficultyLevels: ['N/A', 'low', 'medium', 'high'],
   });
 });
 
