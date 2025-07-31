e9970d3215b3fe42b06c2aac9b4b16a560f0d4e7
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-07-31 11:09:10 +0400

    bookmark fix

--- api/routes/bookmark.route.js
+++ api/routes/bookmark.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken, optionalVerifyToken } from '../utils/verifyUser.js';
 import {
   addBookmark,
   removeBookmark,
@@ -9,9 +9,12 @@ import {
 
 const router = express.Router();
 
+// These routes require authentication
 router.post('/add', verifyToken, addBookmark);
 router.delete('/remove/:postId', verifyToken, removeBookmark);
 router.get('/posts', verifyToken, getBookmarkedPosts);
-router.get('/status/:postId', verifyToken, checkBookmarkStatus);
+
+// This route works for both logged in and guest users
+router.get('/status/:postId', optionalVerifyToken, checkBookmarkStatus);
 
 export default router;
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
+
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) {
@@ -9,6 +10,26 @@ export const verifyToken = (req, res, next) => {
         if (err) {
             return next(errorHandler(401, 'Unauthorized'));
         }
+        req.user = user;
+        next();
+    });
+};
+
+// Optional token verification - doesn't fail if no token
+export const optionalVerifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    
+    if (!token) {
+        // No token - continue without setting req.user
+        return next();
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            // Invalid token - continue without setting req.user
+            return next();
+        }
+        
         req.user = user;
         next();
     });
